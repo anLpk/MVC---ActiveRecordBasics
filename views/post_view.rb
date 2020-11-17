4f02ca2ca1c8c83fafd758c7843f38e5a77cee88
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-11-17 21:57:39 +1100

    Update post_view.rb

--- views/post_view.rb
+++ views/post_view.rb
@@ -1,3 +1,18 @@
 class PostsView
   # TODO: implement some methods here when controller needs to `puts` or `gets`
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id} -\nTitle: #{post.title}\nURL: #{post.url}\nVotes: #{post.votes}\n\n"
+    end
+  end
+
+  def ask_user_for(label)
+    puts "Please enter the #{label}"
+    gets.chomp
+  end
+
+  def ask_user_for_vote
+    puts "Please enter the  vote"
+    gets.chomp.to_i
+  end
 end
